6519a4649f936c69d0a3083db36f4a0c74c4ed57
omotoshoayomikun <omotoshoayomikun@gmail.com>
2024-09-19 17:09:42 +0100

    Update

--- src/components/Duration/Duration.jsx
+++ src/components/Duration/Duration.jsx
@@ -25,6 +25,77 @@ function Duration() {
           Cars and Household goods with containers. "Call us for price"
         </p>
       </div>
+
+      <div className="side-padding my-[10px] pb-2">
+        <h1 className="head_text text-center">Messager Barrels</h1>
+        <p className="sm:text-[33px] text-[20px]">
+          We can also assist to buy what you need and ship them with 9ja
+          Barrels. Please give us a call for this service
+        </p>
+      </div>
+
+      <div className="py-6 bg-[--bg--bg] mt-6">
+        <div className="side-padding my-[10px] pb-2">
+          <h1 className="head_text text-center">9ja barrels advantages.</h1>
+          <ul className="">
+            <li className="sm:text-[33px] text-[20px]">
+              1. No hustling with luggages, overweight at the airport.
+            </li>
+            <li className="sm:text-[33px] text-[20px]">
+              2. Cost efficient on heavy loads.2. Deliver Barrel to our shopping
+              yard
+            </li>
+            <li className="sm:text-[33px] text-[20px]">
+              3. Clean up the garage and ship unused stuff to Nigeria with 9ja
+              barrels.
+            </li>
+            <li className="sm:text-[33px] text-[20px]">
+              4. All unperishable food stuffs can now be ship to Nigeria.
+            </li>
+            <li className="sm:text-[33px] text-[20px]">
+              5. Plan ahead and ship heavy items before traveling.
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <div className="side-padding my-[10px] pb-2">
+        <h1 className="head_text text-center">Barrels can't fit your car? </h1>
+        <p className="sm:text-[33px] text-[20px]">
+          Don't worry, load your stuff in your car and come to buy Barrells at
+          our shipping yards. Barrells costs is $35.
+        </p>
+      </div>
+
+      <div className="py-6 bg-[--bg--bg] mt-6">
+        <div className="side-padding my-[10px] pb-2">
+          <h1 className="head_text text-center">Strictly Prohibited items</h1>
+          <ul className="sm:text-left list-disc ml-auto mr-auto w-[max-content] sm:ml-0 ">
+            <li className="sm:text-[33px] text-[20px]">Guns & Ammunition</li>
+            <li className="sm:text-[33px] text-[20px]">
+              Hazardous materials
+            </li>
+            <li className="sm:text-[33px] text-[20px]">
+              Oils and inflammable
+            </li>
+          </ul>
+        </div>
+        <div className="side-padding">
+        <hr className="w-full border-2 border-dashed" />
+          <p className="sm:text-[33px] text-[20px] text-center mt-5">
+            Be aware,{" "}
+            <span className="">
+              custom agent scan Barrells,
+            </span>{" "}
+            we are not responsible for custom who break the law.
+          </p>
+          <p className="sm:text-[33px] text-[20px] text-center mt-3">
+            We are here to give you our best service.
+          </p>
+
+        </div>
+      </div>
+
       <div className="side-padding my-[10px] pb-2">
         <h1 className="head_text text-center">Business Address</h1>
         <p className="sm:text-[33px] text-[20px] text-center">
